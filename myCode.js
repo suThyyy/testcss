1ee7a0951a7de40e95fb7553740e26f2e3cbf420
suThyy <oqnvuz0704@gmail.com>
2026-09-24 16:18:21 +0700

    fix indicator

--- myCode.js
+++ myCode.js
@@ -1,3 +1,33 @@
 function Showtable() {
   document.getElementById("promotion-table").style.display = "block";
 }
+const slides = document.querySelectorAll(".item");
+const indicators = document.querySelectorAll(".indicator");
+let currentSlide = 0;
+function showSlide(n) {
+  if (n >= slides.length) currentSlide = 0;
+  else if (n < 0) currentSlide = slides.length - 1;
+  else currentSlide = n;
+  slides.forEach((slide) => slide.classList.remove("active"));
+  slides[currentSlide].classList.add("active");
+
+  indicators.forEach((indicator) => indicator.classList.remove("active"));
+  indicators[currentSlide].classList.add("active");
+}
+
+function changeSlide(direction) {
+  showSlide(currentSlide + direction);
+}
+
+document
+  .getElementById("prevBtn")
+  .addEventListener("click", () => changeSlide(-1));
+document
+  .getElementById("nextBtn")
+  .addEventListener("click", () => changeSlide(1));
+
+indicators.forEach((indicator) => {
+  indicator.addEventListener("click", () =>
+    showSlide(Number(indicator.dataset.slideTo)),
+  );
+});
